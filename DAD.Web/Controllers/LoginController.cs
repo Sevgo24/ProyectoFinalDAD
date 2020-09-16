051c2258639f3bf036e2b0fffd12794a60633a51
Ronald Jesus Cordova Gonzales <ronaldcordovag@bcp.com.pe>
2020-09-16 10:55:23 -0500

    cambios

--- DAD.Web/Controllers/LoginController.cs
+++ DAD.Web/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAD.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,12 @@ namespace DAD.Web.Controllers
     {
         // GET: Login
         public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Login )
         {
             return View();
         }
